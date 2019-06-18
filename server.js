a8a88b5e8d38b19f5e10927df7dee99a328d3203
manoj997 <dharmakumar338@gmail.com>
2019-06-18 17:53:08 +0530

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+const http=require('http');
+const express=require('express');
+const app =express();
+const port=process.env.PORT || 5000;
+
+app.get('/', function(req,res){
+    res.send('<h1>Hello Manoj!</h1>');
+});
+
+app.listen(port,()=>{
+    console.log('Server is running on port: ' + port);
+});
+
