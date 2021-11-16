d24ed5599dea4293485e1f3607a8e696d39b2d8a
Manoj Kumar <dharmakumar338@gmail.com>
2021-11-16 22:14:35 +0530

    bcrypt and other updated packages

--- controllers/Instructor/InstructorController.js
+++ controllers/Instructor/InstructorController.js
@@ -1,186 +1,202 @@
-const Instructor=require('../../models/Instructor/Instructor');
-const mongoose=require('mongoose');
-const jwt=require('jsonwebtoken');
-const bcrypt=require('bcrypt');
+const Instructor = require("../../models/Instructor/Instructor");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 //const checkAuthAdmin=require('../../auth/checkAuthInstructor');
-const nodemailer=require('nodemailer');
-const keys=require('../../config/keys');
-const JWT_KEY=keys.JWT_KEY;
+const nodemailer = require("nodemailer");
+const keys = require("../../config/keys");
+const JWT_KEY = keys.JWT_KEY;
 
-exports.getAllInstructor=(req,res,next)=>{
-    Instructor.find(function (err,id) {
-        if(err){
-            console.log(err);
-        }else{
-            res.json(id);
-        }
-    });
+exports.getAllInstructor = (req, res, next) => {
+  Instructor.find(function (err, id) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(id);
+    }
+  });
 };
 
-exports.addInstructor=(req,res,next)=>{
-    Instructor.find({
-        instructorID: req.body.instructorID
-    }).exec()
-        .then(instructor =>{
-            if(instructor.length>=1){
-                res.status(409).json({
-                    message:'Instructor already exists'
-                });
-            }else {
-                bcrypt.hash(req.body.password,10,(err,hash)=>{
-                    if(err){
-                        return res.status(500).json({
-                            error:err
-                        });
-                    }else{
-                        const instructor =new Instructor({
-                            _id:mongoose.Types.ObjectId(),
-                            instructorID:req.body.instructorID,
-                            name: req.body.name,
-                            email:req.body.email,
-                            password:hash,
-                            userType: 'instructor'
-                        });
-
-                        instructor.save().then(result=>{
-                            //send email to instructor email
-                            const transporter=nodemailer.createTransport({
-                                service:'gmail',
-                                auth:{
-                                    user:'stu2019info@gmail.com',
-                                    pass:'ljxgjhhfuvotignp'
-                                }
-                            });
-
-                            const mailOptions={
-                                from:'noreply@eventors4.herokuapp.com',
-                                to:instructor.email,
-                                subject:'Welcome '+ instructor.name +' to Student Information System!',
-                                html:'Hi! '+instructor.name +', <br/> '+ 'Welcome to Student Information System. We warmly welcome our instructor!<br/> Thanks and Best Regards,<br/>Admin'
-                            };
-
-                            transporter.sendMail(mailOptions,(error,info)=>{
-                                if(error){
-                                    console.log(error);
-                                }else{
-                                    console.log('Email sent:'+info.response);
-                                }
-                            });
-
-                            console.log(result);
-                            res.status(201).json({
-                                message:'Instructor added',
-                                createdInstructor:result
-                            });
-                        }).catch(err=>{
-                            console.log(err.message);
-                            res.status(500).json({
-                                error:err
-                            });
-                        });
-                    }
-                });
-
-            }
-        })
-
-};
-
-exports.findInstructorByCode=(req,res,next)=>{
-    Instructor.find({
-        instructorID:req.params.instructorID
-    }).exec().then(result=>{
-        if(result.length>=1){
-            res.status(200).json({
-                instructor:result
-            });
-
-        }else{
-            res.status(404).json({
-                message:'Nothing was found here!'
-            });
-        }
-    }).catch(err=>{
-        res.status(500).json({
-            error:err
+exports.addInstructor = (req, res, next) => {
+  Instructor.find({
+    instructorID: req.body.instructorID,
+  })
+    .exec()
+    .then((instructor) => {
+      if (instructor.length >= 1) {
+        res.status(409).json({
+          message: "Instructor already exists",
         });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const instructor = new Instructor({
+              _id: mongoose.Types.ObjectId(),
+              instructorID: req.body.instructorID,
+              name: req.body.name,
+              email: req.body.email,
+              password: hash,
+              userType: "instructor",
+            });
+
+            instructor
+              .save()
+              .then((result) => {
+                //send email to instructor email
+                const transporter = nodemailer.createTransport({
+                  service: "gmail",
+                  auth: {
+                    user: "stu2019info@gmail.com",
+                    pass: "ljxgjhhfuvotignp",
+                  },
+                });
+
+                const mailOptions = {
+                  from: "noreply@eventors4.herokuapp.com",
+                  to: instructor.email,
+                  subject:
+                    "Welcome " +
+                    instructor.name +
+                    " to Student Information System!",
+                  html:
+                    "Hi! " +
+                    instructor.name +
+                    ", <br/> " +
+                    "Welcome to Student Information System. We warmly welcome our instructor!<br/> Thanks and Best Regards,<br/>Admin",
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    console.log("Email sent:" + info.response);
+                  }
+                });
+
+                console.log(result);
+                res.status(201).json({
+                  message: "Instructor added",
+                  createdInstructor: result,
+                });
+              })
+              .catch((err) => {
+                console.log(err.message);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
     });
 };
 
-exports.deleteInstructorByCode=(req,res,next)=>{
-
-    Instructor.remove({instructorID:req.params.instructorID},function (err,instructor) {
-            if(err)
-                res.status(400).json(err);
-
-            else
-                res.status(200).json('Successfully removed')
+exports.findInstructorByCode = (req, res, next) => {
+  Instructor.find({
+    instructorID: req.params.instructorID,
+  })
+    .exec()
+    .then((result) => {
+      if (result.length >= 1) {
+        res.status(200).json({
+          instructor: result,
+        });
+      } else {
+        res.status(404).json({
+          message: "Nothing was found here!",
+        });
+      }
     })
-};//
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
+exports.deleteInstructorByCode = (req, res, next) => {
+  Instructor.remove(
+    { instructorID: req.params.instructorID },
+    function (err, instructor) {
+      if (err) res.status(400).json(err);
+      else res.status(200).json("Successfully removed");
+    }
+  );
+}; //
 
 //instructorLogin
-exports.instructor_signIn=(req,res,next)=>{
-    Instructor.find({instructorID:req.body.instructorID})
-        .exec()
-        .then(instructor=>{
-            if(instructor.length<1){
-                return res.status(401).json({
-                    message:'Authorization Failed!'
-                });
-            }
-            bcrypt.compare(req.body.password,instructor[0].password,(err,result)=>{
-                if(err){
-                    return res.status(401).json({
-                        message:'Incorrect Password'
-                    });
-                }
-                if(result){
-                    //correct password
-                    const token=jwt.sign({
-                            id:instructor[0]._id,
-                            instructorID:instructor[0].instructorID,
-                            userType:instructor[0].userType
-
-                        },
-                        JWT_KEY,
-                        {
-                            expiresIn: "1h"
-                        }
-                    );
-                    console.log(instructor);
-                    return res.status(200).json({
-                        message:'Authorization Success',
-                        token:token
-                    });
-                }
-                res.status(401).json({
-                    message:'Authorization Failed!'
-                });
-            });
-        }).catch(err=>{
-        console.log(err);
-        res.status(500).json({
-            error:err
+exports.instructor_signIn = (req, res, next) => {
+  Instructor.find({ instructorID: req.body.instructorID })
+    .exec()
+    .then((instructor) => {
+      if (instructor.length < 1) {
+        return res.status(401).json({
+          message: "Authorization Failed!",
         });
+      }
+      bcrypt.compare(
+        req.body.password,
+        instructor[0].password,
+        (err, result) => {
+          if (err) {
+            return res.status(401).json({
+              message: "Incorrect Password",
+            });
+          }
+          if (result) {
+            //correct password
+            const token = jwt.sign(
+              {
+                id: instructor[0]._id,
+                instructorID: instructor[0].instructorID,
+                userType: instructor[0].userType,
+              },
+              JWT_KEY,
+              {
+                expiresIn: "1h",
+              }
+            );
+            console.log(instructor);
+            return res.status(200).json({
+              message: "Authorization Success",
+              token: token,
+            });
+          }
+          res.status(401).json({
+            message: "Authorization Failed!",
+          });
+        }
+      );
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
 
-exports.instructor_update=(req,res,next)=>{
-    const id=req.params.instructorID;
+exports.instructor_update = (req, res, next) => {
+  const id = req.params.instructorID;
 
-    const updatedInstructor={
-        name:req.body.name,
-        email:req.body.email,
-        password: req.body.password,
-    };
-    Instructor.update({instructorID:id},updatedInstructor)
-        .exec().then(result=>{
-        res.send(result);
-    }).catch(err=>{
-        res.status(500).json({
-            message:'Error update failed!',
-            error:err
-        })
+  const updatedInstructor = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  Instructor.update({ instructorID: id }, updatedInstructor)
+    .exec()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Error update failed!",
+        error: err,
+      });
     });
 };
--- controllers/Student/StudentController.js
+++ controllers/Student/StudentController.js
@@ -1,191 +1,198 @@
-const StudentModel = require('../../models/Student/Student');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt=require('jsonwebtoken');
-const checkAuth=require('../../auth/check-auth');
-const keys=require('../../config/keys');
-const JWT_KEY=keys.JWT_KEY;
+const StudentModel = require("../../models/Student/Student");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const checkAuth = require("../../auth/check-auth");
+const keys = require("../../config/keys");
+const JWT_KEY = keys.JWT_KEY;
 
 //POST
-exports.students_signup = (req, res, next)=>{
-    StudentModel.find({
-        studentID: req.body.studentID
-    }).exec().then(student => {
-        if(student.length >= 1){
-            res.status(409).json({
-                message: 'Student ID exists!'
+exports.students_signup = (req, res, next) => {
+  StudentModel.find({
+    studentID: req.body.studentID,
+  })
+    .exec()
+    .then((student) => {
+      if (student.length >= 1) {
+        res.status(409).json({
+          message: "Student ID exists!",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
             });
-        }
-        else {
-            bcrypt.hash(req.body.password,10,(err,hash)=>{
-                if(err){
-                    return res.status(500).json({
-                        error: err
-                    });
-                }
-                else {
-                    const studentType="student";
-                    const new_student = new StudentModel({
-                        _id : mongoose.Types.ObjectId(),
-                        studentID: req.body.studentID,
-                        name: req.body.name,
-                        email: req.body.email,
-                        password: hash,
-                        userType: studentType,
-                        enrolledCourse:req.body.enrolledCourse
-                    });
+          } else {
+            const studentType = "student";
+            const new_student = new StudentModel({
+              _id: mongoose.Types.ObjectId(),
+              studentID: req.body.studentID,
+              name: req.body.name,
+              email: req.body.email,
+              password: hash,
+              userType: studentType,
+              enrolledCourse: req.body.enrolledCourse,
+            });
 
-                    new_student.save().then(result=>{
-                        console.log(result);
+            new_student
+              .save()
+              .then((result) => {
+                console.log(result);
 
-                        res.status(201).json({
-                            message:'New Student Added!'
-                        });
-                    }).catch(err=>{
-                        console.log(err);
+                res.status(201).json({
+                  message: "New Student Added!",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
 
-                        res.status(500).json({
-                            error:err
-                        });
-                    });
-                }
-            })
-        }
-    })
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
 };
-
 
 //GET - Specified Students
-exports.get_studentById= (req,res,next)=>{
-    StudentModel.find({
-        studentID:req.params.studentID
-    })
-        .exec().then(result => {
-            if(result.length >= 1){
-                res.status(200).json({
-                    student:result
-                });
-            }else {
-                res.status(404).json({
-                    message: 'Student ID does not match!!'
-                });
-            }
-    }).catch(err => {
-        res.status(500).json({
-            error:err
+exports.get_studentById = (req, res, next) => {
+  StudentModel.find({
+    studentID: req.params.studentID,
+  })
+    .exec()
+    .then((result) => {
+      if (result.length >= 1) {
+        res.status(200).json({
+          student: result,
         });
+      } else {
+        res.status(404).json({
+          message: "Student ID does not match!!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
 };
 
-exports.admin_update=(req,res,next)=>{
-    const id=req.params.studentID;
+exports.admin_update = (req, res, next) => {
+  const id = req.params.studentID;
 
-    const updatedStudent={
-        name:req.body.name,
-        email:req.body.email,
-        password: req.body.password,
-    };
-    StudentModel.update({studentID:id},updatedStudent)
-        .exec().then(result=>{
-        res.send(result);
-        alert('add done')
-    }).catch(err=>{
-        res.status(500).json({
-            message:'Error update failed!',
-            error:err
-        })
+  const updatedStudent = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  StudentModel.update({ studentID: id }, updatedStudent)
+    .exec()
+    .then((result) => {
+      res.send(result);
+      alert("add done");
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Error update failed!",
+        error: err,
+      });
     });
 };
 
-
-exports.getAllStudents=(req,res,next)=>{
-    StudentModel.find(function (err,id) {
-        if(err){
-            console.log(err);
-        }else{
-            res.json(id);
-        }
-    });
+exports.getAllStudents = (req, res, next) => {
+  StudentModel.find(function (err, id) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(id);
+    }
+  });
 };
 
 //DELETE - usingStudentID
-exports.delete_byStudentID = (req,res,next)=>{
-    StudentModel.findByIdAndDelete(req.params.studentID).exec().then(result=>{
-        res.status(200).json({
-            message: 'Successfully deleted!!',
-            deletedStuRecord: result
-        });
-    }).catch(err=>{
-        console.log(err.message);
-        res.status(500).json({
-            error:err
-        });
-    });
-
-};
-
-
-
-exports.student_signIn=(req,res,next)=>{
-    StudentModel.find({studentID:req.body.studentID})
-        .exec()
-        .then(student=>{
-            if(student.length<1){
-                return res.status(401).json({
-                    message:'Authorization Failed!'
-                });
-            }
-            bcrypt.compare(req.body.password,student[0].password,(err,result)=>{
-                if(err){
-                    return res.status(401).json({
-                        message:'Incorrect Password'
-                    });
-                }
-                if(result){
-                    //correct password
-                    const token=jwt.sign({
-                        id:student[0]._id,
-                        studentID:student[0].studentID,
-                        userType:student[0].userType
-
-                    },
-                        JWT_KEY,
-                        {
-                            expiresIn: "1h"
-                        }
-                    );
-                    console.log(student);
-                    return res.status(200).json({
-                        message:'Authorization Success',
-                        token:token
-                    });
-                }
-                res.status(401).json({
-                    message:'Authorization Failed!'
-                });
-            });
-        }).catch(err=>{
-            console.log(err);
-            res.status(500).json({
-                error:err
-        });
+exports.delete_byStudentID = (req, res, next) => {
+  StudentModel.findByIdAndDelete(req.params.studentID)
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Successfully deleted!!",
+        deletedStuRecord: result,
+      });
     })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
 
+exports.student_signIn = (req, res, next) => {
+  StudentModel.find({ studentID: req.body.studentID })
+    .exec()
+    .then((student) => {
+      if (student.length < 1) {
+        return res.status(401).json({
+          message: "Authorization Failed!",
+        });
+      }
+      bcrypt.compare(req.body.password, student[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Incorrect Password",
+          });
+        }
+        if (result) {
+          //correct password
+          const token = jwt.sign(
+            {
+              id: student[0]._id,
+              studentID: student[0].studentID,
+              userType: student[0].userType,
+            },
+            JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          console.log(student);
+          return res.status(200).json({
+            message: "Authorization Success",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Authorization Failed!",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
-exports.student_enroll=(req,res,next)=>{
-    const id=req.body.id;
+exports.student_enroll = (req, res, next) => {
+  const id = req.body.id;
 
-    const enrollment={
-        enrolledCourse: req.params.enrolledCourse
-    };
-    StudentModel.update({_id:id},enrollment)
-        .exec().then(result=>{
-            res.send(result);
-    }).catch(err=>{
-        res.status(500).json({
-            message:'Error update failed!',
-            error:err
-        })
+  const enrollment = {
+    enrolledCourse: req.params.enrolledCourse,
+  };
+  StudentModel.update({ _id: id }, enrollment)
+    .exec()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Error update failed!",
+        error: err,
+      });
     });
 };
